ff469b3d2bda2bb4ff01d4686072e3c60b2b55f5
Sergei Borovskii <serezhka95@gmail.com>
2024-01-10 16:10:19 +0600

    Create rss_parser.py
    feature: rss_parser function was added

---
+++ rss_parser.py
@@ -0,0 +1,30 @@
+from typing import List, Optional
+
+
+def rss_parser(
+    xml: str,
+    limit: Optional[int] = None,
+    json: bool = False,
+) -> List[str]:
+    """
+    RSS parser.
+
+    Args:
+        xml: XML document as a string.
+        limit: Number of the news to return. if None, returns all news.
+        json: If True, format output as JSON.
+
+    Returns:
+        List of strings.
+        Which then can be printed to stdout or written to file as a separate lines.
+
+    Examples:
+        >>> xml = '<rss><channel><title>Some RSS Channel</title><link>https://some.rss.com</link><description>Some RSS Channel</description></channel></rss>'
+        >>> rss_parser(xml)
+        ["Feed: Some RSS Channel",
+        "Link: https://some.rss.com"]
+        >>> print("\\n".join(rss_parser(xmls)))
+        Feed: Some RSS Channel
+        Link: https://some.rss.com
+    """
+    pass
