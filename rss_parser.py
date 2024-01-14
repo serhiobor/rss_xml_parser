accbddc704f0fffc36e0b8884c9dabc080c15c0d
Sergei Borovskii <serezhka95@gmail.com>
2024-01-14 13:00:21 +0600

    fix: parse of xml without items
    - 2 regex for 2 situations
    - logic depense on xml_has_items

--- rss_parser.py
+++ rss_parser.py
@@ -9,7 +9,7 @@ def rss_parser(
     xml: str,
     limit: Optional[int] = None,
     json: bool = False,
-) -> List[str]:
+) -> str:
     """
     RSS parser.
 
@@ -21,44 +21,7 @@ def rss_parser(
     Returns:
         List of strings.
         Which then can be printed to stdout or written to file as a separate lines.
-
-    Examples:
-        >>> xml = '<rss><channel><title>Some RSS Channel</title><link>https://some.rss.com</link><description>Some RSS Channel</description></channel></rss>'
-        >>> rss_parser(xml)
-        ["Feed: Some RSS Channel",
-        "Link: https://some.rss.com"]
-        >>> print("\\n".join(rss_parser(xmls)))
-        Feed: Some RSS Channel
-        Link: https://some.rss.com
     """
-
-    xml = html.unescape(xml)
-    feed_regex = "<channel>(.*?)<item>"
-    feed_string = re.findall(feed_regex, xml, flags=re.DOTALL)[0]
-    items_string = re.split("(</*channel>)", xml)[2]
-
-    feed_elem_order = {
-        'title': 'Feed: ',
-        'link': 'Link: ',
-        'lastBuildDate': 'Last Build Date: ',
-        'pubDate': 'Publish Date: ',
-        'language': 'Language: ',
-        # TODO: check formating of categories
-        'category': 'Categories: ',
-        'managinEditor': 'Editor: ',
-        'description': 'Description: ',
-        'item': 'Items:'
-    }
-
-    item_elem_order = {
-        'title': 'Title: ',
-        'author': 'Author: ',
-        'pubDate': 'Published: ',
-        'link': 'Link: ',
-        # TODO: check formating
-        'category': 'Categories: ',
-        'description': '\n'
-    }
 
     def tags_to_dict(xml_string: str, tags_order):
         tags_dict = OrderedDict()
@@ -82,7 +45,6 @@ def rss_parser(
                 tag_value = ', '.join(tag_value)
             string = f'{tag_name}{tag_value}\n'
             string_to_output += string
-        string_to_output += '\n'
         return string_to_output
 
     def get_feed_info():
@@ -98,21 +60,51 @@ def rss_parser(
                 tags_to_dict(item, item_elem_order.keys()))
         return list_of_items_dict
 
+    xml = html.unescape(xml)  # remove special chars (&amp ect.)
+    feed_elem_order = {
+        'title': 'Feed: ',
+        'link': 'Link: ',
+        'lastBuildDate': 'Last Build Date: ',
+        'pubDate': 'Publish Date: ',
+        'language': 'Language: ',
+        'category': 'Categories: ',
+        'managinEditor': 'Editor: ',
+        'description': 'Description: ',
+        'item': 'Items:'
+    }  # to formatting of feed-info output
+    item_elem_order = {
+        'title': 'Title: ',
+        'author': 'Author: ',
+        'pubDate': 'Published: ',
+        'link': 'Link: ',
+        'category': 'Categories: ',
+        'description': '\n'
+    }  # to formatting of items-info output
+    xml_has_items = '<item>' in xml
+
+    feed_regex = ("<channel>(.*?)(</channel>)", "<channel>(.*?)<item>")[
+        xml_has_items]  # depends on the presence of items in xml
+    feed_string = re.search(feed_regex, xml, flags=re.DOTALL).group()
+
+    items_string = None
+    if xml_has_items:
+        items_string = re.split("(</*channel>)", xml)[2]
+
     if json:
         dict_to_output = get_feed_info()
-        dict_to_output['items'] = get_items()
+        if xml_has_items:
+            dict_to_output['items'] = get_items()
         return jsn.dumps(dict_to_output, indent=2)
 
-    else:
-        string_to_output = ''
-
-        feed_info = get_feed_info()
-        feed_string = get_output_string(feed_info, feed_elem_order)
-        string_to_output += feed_string
-
+    string_to_output = ''
+    feed_info = get_feed_info()
+    feed_string = get_output_string(feed_info, feed_elem_order)
+    string_to_output += feed_string
+    if xml_has_items:
+        string_to_output += '\n'
         items = get_items()
         for item in items:
             item_string = get_output_string(item, item_elem_order)
             string_to_output += item_string
-
-        return string_to_output
+            string_to_output += '\n'
+    return string_to_output[:-2]
